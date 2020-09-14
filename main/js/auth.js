9dd697b20c846387c9aed8f89976d864bf928bf3
Mihir Kumar Singh <mihirkumar02@gmail.com>
2020-09-14 21:06:03 +0530

    added phone number SIGNIN - FIREBASE AUTH

--- main/js/auth.js
+++ main/js/auth.js
@@ -66,3 +66,46 @@ function googleLogin(){
         window.alert(errorMessage);
     })
 }
+
+/* PHONE NUMBER LOGIN Code */
+
+// Variable to store captchaVerification result
+var resultCode;
+
+// Render Captcha box when page loads
+window.onload = function(){
+    captchaRender();
+};
+function captchaRender(){
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+    recaptchaVerifier.render();
+}
+
+// =====================================
+
+// Get the phone number from user
+function getPhoneNumberFromUserInput(){
+    var phoneNumber = document.getElementById('phone').value;
+
+    firebase.auth().signInWithPhoneNumber(phoneNumber, window.recaptchaVerifier)
+        .then(function (confirmationResult) {
+          window.confirmationResult = confirmationResult;
+          resultCode = confirmationResult;
+          window.alert('Code sent to ' + phoneNumber);
+        }).catch(function (error) {
+          window.alert(error.message);
+    });
+}
+
+// ======================================
+
+// Verify the code entered by user
+function verifyCode(){
+    var code = document.getElementById('verificationCode').value;
+    resultCode.confirm(code).then(function (result) {
+        window.alert("Code Verified!");
+        var user = result.user;
+      }).catch(function (error) {
+        window.alert(error.message);
+      });
+}
